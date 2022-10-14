4b285e6d60ffbba96d9f09f6420c28f44fe45de3
boh <yatsunbohdan194@gmail.com>
2022-10-14 22:21:45 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+	window.addEventListener('scroll', e => {
+		document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`);
+	});
+
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content'
+	});
+});
